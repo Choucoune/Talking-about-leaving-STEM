a1f9ab8e9884211cc1371e7795302c857399f7dc
bkoester <bkoester@bkoester-mbp2.lan>
2021-03-18 09:51:32 -0400

    physics major example

---
+++ count_physics_majors.R
@@ -0,0 +1,24 @@
+count_physics_majors <- function(sc,sr)
+{
+
+  library(tidyverse)
+  #sc <- read_tsv("/Users/bkoester/Box Sync/LARC.WORKING/BPK_LARC_STUDENT_COURSE_20210209.tab")
+  #sr <- read_tsv("/Users/bkoester/Box Sync/LARC.WORKING/BPK_LARC_STUDENT_RECORD_20210209.tab") 
+ 
+  n_physics <- sr %>% filter(UM_DGR_1_MAJOR_1_DES  == 'Physics BS') %>% select(STDNT_ID,UM_DGR_1_MAJOR_1_DES,FIRST_TERM_ATTND_SHORT_DES)
+  phys_course <- sc %>% filter(SBJCT_CD == 'PHYSICS' & CATLG_NBR == 140) %>% select(STDNT_ID,TERM_SHORT_DES,GRD_PNTS_PER_UNIT_NBR)
+  
+  out <- n_physics %>% left_join(phys_course,by='STDNT_ID')
+  print(out %>% filter(TERM_SHORT_DES != 'NA') %>% tally())
+  
+  phys_course <- sc %>% filter(SBJCT_CD == 'PHYSICS' & CATLG_NBR == 160) %>% select(STDNT_ID,TERM_SHORT_DES,GRD_PNTS_PER_UNIT_NBR)
+  out <- n_physics %>% left_join(phys_course,by='STDNT_ID')
+  print(out %>% filter(TERM_SHORT_DES != 'NA') %>% tally())
+  
+  phys_course <- sc %>% filter(SBJCT_CD == 'PHYSICS' & CATLG_NBR == 135) %>% select(STDNT_ID,TERM_SHORT_DES,GRD_PNTS_PER_UNIT_NBR)
+  out <- n_physics %>% left_join(phys_course,by='STDNT_ID')
+  print(out %>% filter(TERM_SHORT_DES != 'NA') %>% tally())
+  
+  return(out)
+   
+}
